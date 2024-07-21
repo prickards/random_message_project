a4d8c76002ab943664194ef9cbf0dd578a6cc0fa
prickards <petesbusiness247@gmail.com>
2024-07-21 12:29:52 +0930

    Characters added as an array to be selected from at random to insult the user

--- rnd_msg.js
+++ rnd_msg.js
@@ -1,11 +1,23 @@
+const insultor = [{name: 'Jim Halpert', job: 'sales man', insult: 'lazy'}, 
+    {name: 'Dwight Schroot', job: 'sales man', insult: 'weak'},
+    {name: 'Michael Scott', job: 'The boss', insult: 'not funny'},
+    {name: 'Kevin Malone', job: 'Accountant', insult: 'Dumb'},
+    {name: 'Pam Beasly', job: 'Receptionist', insult: 'Too nice'},
+    {name: 'Toby Flenderson', job: 'HR rep', insult: 'Rule breaker'},
+    {name: 'Kelly Kapoor', job: 'Customer services', insult: 'Too chatty'},
+    {name: 'Creed Braton', job: 'Qaulity control', insult: 'Creepy'},
+    {name: 'Ryan Howard', job: 'Intern', insult: 'protentious'},
+    {name: 'Mose', job: 'Beet farmer', insult: 'Your have not beets'}
+]
+
 const theInsultor = name => {
     
 //variables to be used in switch statement and final outputted message. 
-    let rndNum = Math.floor(Math.random() * 10); 
+    let rndIn = Math.floor(Math.random() * 10); 
     let insult = "fat"
 
 //this switch statement will randomly assign the user an insult to be used later in the outputted message. 
-    switch (rndNum) {
+    switch (rndIn) {
         case 1: 
         insult = "fat"; 
         break; 
