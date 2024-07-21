f1eb8f1b78712fc2877204f92b14083090e45d8c
prickards <petesbusiness247@gmail.com>
2024-07-21 13:04:39 +0930

    final draft of project

--- rnd_msg.js
+++ rnd_msg.js
@@ -1,62 +1,66 @@
 //An array of character from which one will be randomly selected to insult the user.
 const insultor = [{name: 'Jim Halpert', job: 'Sales man', insult: 'lazy'}, 
     {name: 'Dwight Schroot', job: 'Sales man', insult: 'weak'},
-    {name: 'Michael Scott', job: 'The boss', insult: 'not funny'},
     {name: 'Kevin Malone', job: 'Accountant', insult: 'dumb'},
+    {name: 'Michael Scott', job: 'The boss', insult: 'not funny'},
     {name: 'Pam Beasly', job: 'Receptionist', insult: 'too nice'},
     {name: 'Toby Flenderson', job: 'HR rep', insult: 'rebelious'},
     {name: 'Kelly Kapoor', job: 'Customer services', insult: 'too chatty'},
     {name: 'Creed Braton', job: 'Qaulity control', insult: 'creepy'},
     {name: 'Ryan Howard', job: 'Intern', insult: 'protentious'},
-    {name: 'Mose', job: 'Beet farmer', insult: 'your have not beets'}
+    {name: 'Mose', job: 'Beet farmer', insult: 'beetless'}
 ]
 
 const theInsult = name => {
     
 //variables to be used in switch statement and final outputted message. 
     let rndNum = Math.floor(Math.random() * 10); 
-    let character = insultor[rndNum][0]
+    let character = insultor[rndNum].name
+    let insultSelect = insultor[rndNum].insult
     let animal = "Sloth"
 
-//this switch statement will randomly assign the user an insult to be used later in the outputted message. 
+//this switch statement will randomly assign the user an animal to be used later in the outputted message. 
     switch (rndNum) {
-        case 1: 
+        case 0: 
         animal = "sloth"; 
         break; 
-        case 2: 
+        case 1: 
         animal = "mouse"; 
         break; 
-        case 3: 
-        animal = "toby"
+        case 2: 
+        animal = "pig"
         break;
+        case 3: 
+        animal = "toby" 
+        break; 
         case 4: 
-        insult = "pig" 
+        animal = "quaker"; 
         break; 
         case 5: 
-        insult = "quaker"; 
+        animal = "cat"
         break; 
         case 6: 
-        insult = "cat"
+        animal = "bird"; 
         break; 
-        case 7: 
-        insult = "bird"; 
+        case 7:
+        animal = "naked mole rat"; 
         break; 
-        case 8:
-        insult = "naked mole rat"; 
-        break; 
-        case 9: 
-        insult = "turtle"
+        case 8: 
+        animal = "turtle"
         break;
-        case 10:
-        insult = "horse"; 
+        case 9:
+        animal = "horse"; 
         break; 
         default: 
-        insult = "sloth"; 
+        animal = "sloth"; 
         break; 
     }
 
-    console.log(`Hi ${name}, I have been speaking to ${character} and we both think that your are ${insult} just like an ${animal}`); 
+    console.log(`Hi ${name}, I have been speaking to ${character} and we both think that your are ${insultSelect} just like a ${animal}`); 
 }
 
-theInsult('Pete'); 
-
+theInsult('Charles'); 
+theInsult('Amy');
+theInsult('Terry');
+theInsult('Holt');
+theInsult('Jake');
